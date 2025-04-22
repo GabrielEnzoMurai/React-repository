636d69199e11902d80800d51561d4d23695b0b08
windows <gabriel.enzom@gmail.com>
2025-04-22 14:18:30 -0300

    Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -17,7 +17,7 @@ function Footer () {
             <div className="container">
                 <div className='d-flex jc-space-between'>
                     <div className='footer-logo-col'>
-                        <img src={Logo} />
+                        <img src={Logo} className='footer-logo'/>
                         <p className='grey-1-color'>A escola que prepara você para as profissões em alta no mercado de trabalho.</p>
                         <div className='d-flex social-links'>
                             <a href="https://google.com" target="_blank">
@@ -33,6 +33,30 @@ function Footer () {
                                 <img src={InstagramLogo} />
                             </a>
                         </div>
+                    </div>
+                    <div className='d-flex'>
+                        <div className='footer-col'>
+                            <h3>Pages</h3>
+                            <ul>
+                                <li><Link to="/">Home</Link></li>
+                                <li><Link to="/about">About</Link></li>
+                                <li><Link to="/projects">Projects</Link></li>
+                                <li><Link to="/contact">Contact</Link></li>
+                            </ul>
+                        </div>
+                        <div className='footer-col'>
+                            <h3>Contact</h3>
+                            <p className='grey-1-color'>R. Justino Cobra, 61 – Vila Ema | São José dos Campos – SP | CEP 12243-030 </p>
+                            <p className='grey-1-color'>suporte@escoladnc.com.br</p>
+                            <p className='grey-1-color'>(19) 99187-4342</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='d-flex jc-space-between footer-copy'>
+                    <p className='grey-1-color'>Copyright © DNC - 2024</p>
+                    <div className='langs-area d-flex'>
+                        <img src={BrazilLogo} height="29px" />
+                        <img src={UsaLogo} height="29px" />
                     </div>
                 </div>
             </div>
