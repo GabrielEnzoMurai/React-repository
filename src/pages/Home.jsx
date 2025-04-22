4d5abf3aed0048676e8e7ca2a2f6ed702ad832dd
windows <gabriel.enzom@gmail.com>
2025-04-22 12:09:14 -0300

    header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
+import Header from '../components/Header/Header.jsx'
+
 function Home() {
     return (
         <>
+            <Header />
             <h1>Home</h1>
             <p>Subtitle</p>	
         </>
