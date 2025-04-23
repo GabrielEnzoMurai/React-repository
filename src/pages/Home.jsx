04495be06e3fc5954a7834f8f7c8d8aa27a26eb7
windows <gabriel.enzom@gmail.com>
2025-04-23 19:48:56 -0300

    banner add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import Banner from '../components/Banner/Banner'    
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
 import Button from '../components/Button/Button'
@@ -9,6 +10,7 @@ function Home() {
             <h1>Home</h1>
             <p>Subtitle</p>
             <Button buttonStyle="secondary" arrow>Olá</Button>
+            <Banner title="Home" image="About.jpg" />
             <Footer />
         </>
     )
