17ae23b5eb005e11135806b3c7848727b22f1ad5
windows <gabriel.enzom@gmail.com>
2025-04-24 15:07:53 -0300

    projects page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,7 @@
-import Banner from '../components/Banner/Banner'    
 import Header from '../components/Header/Header'
 import Hero from '../components/Hero/Hero'
-import Footer from '../components/Footer/Footer'
-import Button from '../components/Button/Button'
 import ProjectsList from '../components/ProjectsList/ProjectsList'
+import Footer from '../components/Footer/Footer'
 
 function Home() {
     return (
